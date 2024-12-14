f9cd7dedcf2a2c297339f68af238bb1263c1cd61
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-12-14 14:15:14 +0530

    create the basic foundation of blog list

--- app/page.js
+++ app/page.js
@@ -1,5 +1,5 @@
 'use client'
-import BlogItem from "@/Components/BlogItem";
+import BlogList from "@/Components/BlogList";
 import Header from "@/Components/Header";
 
 
@@ -7,7 +7,7 @@ export default function Home() {
   return (
      <>
      <Header/>
-     <BlogItem/>
+     <BlogList/>
      </>
   );
 }
