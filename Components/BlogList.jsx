436e3cb95b4f62accbb87056a353df4d8e162f28
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-12-14 18:56:23 +0530

    design the menu filter

--- Components/BlogList.jsx
+++ Components/BlogList.jsx
@@ -15,7 +15,8 @@ const BlogList = () => {
         <button onClick={()=>setMenu('LifeStyle')} className={menu==="LifeStyle"?'bg-black text-white py-1 px-4 rounded-sm':""}>LifeStyle</button>
       </div>
       <div className='flex flex-wrap justify-around gap-1 gap-y-10 mb-16 xl:mx-24'>
-        {blog_data.map((item,index)=>{
+        {/* filter items when click menu item */}
+        {blog_data.filter((item)=> menu==="All"?true:item.category===menu).map((item,index)=>{
             return <BlogItem key={index} image={item.image} title={item.title} description={item.description} category={item.category}/>
         })}
       </div>
