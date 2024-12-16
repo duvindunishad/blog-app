ea4e490e83b609dc177b46fcc4eeb0186a3849cc
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-12-16 21:33:33 +0530

    create the blog models and db file

--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -1,6 +1,7 @@
 'use client'
 import { assets, blog_data } from '@/Assets/assets';
 import Image from 'next/image';
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
 const page = ({params}) => {
@@ -21,7 +22,9 @@ const page = ({params}) => {
   return ( data?<>
     <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
       <div className="flex justify-between items-center">
+        <Link href='/'>
         <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
+        </Link>
         <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#00000]'>
           Get Start <Image src={assets.arrow} alt=''/>
         </button>
