d75adb9079700f3c463c7dc5dfb9b5ee85d2b7f1
Duvindu Nishad Karunasingha <10748164@students.plymouth.ac.uk>
2024-12-17 00:34:23 +0530

    create the router page and connect with the data base

---
+++ app/api/blog/route.js
@@ -0,0 +1,38 @@
+const { NextResponse } = require("next/server")
+import {writeFile} from 'fs/promises'
+import {ConnectDB} from "../../../lib/config/db"
+import  BlogModel  from "../../../lib/models/BlogModel"
+
+const LoadDB = async () =>{
+    await ConnectDB();
+}
+ LoadDB();
+
+export async function GET(request) {
+    return NextResponse.json({msg:"Api working"})
+}
+
+export async function POST(request) {
+    const formData = await request.formData();
+    const timestamp = Date.now();
+    // use this function to store image in public folder
+
+    const image = formData.get('image');
+    const imageByteData = await image.arrayBuffer();
+    const buffer = Buffer.from(imageByteData);
+    const path = `./public/${timestamp}_${image.name}`;
+    await writeFile(path,buffer);
+    const imgUrl = `/${timestamp}_${image.name}`;
+
+    const blogData = {
+        title: `${formData.get('title')}`,
+        description: `${formData.get('description')}`,
+        category: `${formData.get('category')}`,
+        author: `${formData.get('author')}`,
+        image: `${imgUrl}`,
+        authorImg: `${formData.get('authorImg')}`
+    }
+    await BlogModel.create(blogData);
+    console.log("Blog saved")
+    return NextResponse.json({success:true,msg:"Blog Added"})
+}
